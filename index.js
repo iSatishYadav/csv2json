3f1ec51a315a0dfa8e89bbd03779dacc9a03f6c9
Satish K Yadav (सतीश के यादव) <satishkyadav@corp.bharatpetroleum.com>
2018-10-03 00:23:34 +0530

    writing to fs

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const csvToJson = require('csvtojson');
-
+const fs = require('fs');
 const path = require('path');
 const csvFilePath = path.join(__dirname, 'data', 'customer-data.csv');
 
@@ -13,4 +13,5 @@ const csv2Json = (csvPath = csvFilePath, callback) => {
 csv2Json(process.argv[2], (error, data) => {
     if (error) return console.error(error);
     console.log(`Got this JSON: ${JSON.stringify(data)}`);
+    fs.writeFileSync(path.join(__dirname, 'output', 'customer-data.json'), data, 'utf-8');
 });
